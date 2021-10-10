19ddf59463d78e80be1059eaac6eaa16d1b2ef5d
vikassethi09 <vikassethi2000@gmail.com>
2021-10-10 21:10:11 +0530

    added flexbox

--- src/Components/hero.js
+++ src/Components/hero.js
@@ -1,29 +1,22 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
 
 import Button from 'react-bootstrap/Button'
-
 import './components.css'
 import logo from '../assets/logo.png'
 export default function hero() {
     return (
-        <div  className="hero">
-        <Container >
-        <Row >
-            <Col className="hero-text">
-            <h1 > <span style={{color:"#FD0101"}}>HackClub</span> <br/><span style={{color:"#2A9CEF"}}>CGC</span></h1>
-            <br/>
-            <Button > Join Us</Button>
-            
-            </Col>
-            <Col id="himg">
-            <img src={logo} alt="herologo" className="hero-img"></img>
-            </Col>
-        </Row>
+
+    <div className="hero">
+        <div className="hero-item">
+            <h1 > <span style={{color:"#FD0101"}}>HackClub</span> <br/>
+            <span style={{color:"#2A9CEF"}}>CGC</span>
+            </h1>
   
-    </Container>
-        </div>
+         <Button id="hero-button" > Join Us</Button>
+            </div>
+        <div className="hero-item"><img src={logo} alt="herologo" className="hero-img"></img></div>
+    </div>
+        
     )
 }
