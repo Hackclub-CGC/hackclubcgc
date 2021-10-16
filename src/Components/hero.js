258900daae12466e26946ac2ad6983a600032572
vikassethi09 <vikassethi2000@gmail.com>
2021-10-16 06:49:43 +0530

    typewriter

--- src/Components/hero.js
+++ src/Components/hero.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import Typewriter from 'typewriter-effect'
 import './components.css'
 import logo from '../assets/logo.png'
 export default function hero() {
-    const txtArr =["Grow","Build","Learn","Code"]
-    let changingText;
-    setInterval(() => {for (var i = 0; i < txtArr.length; i++) {
-        changingText = txtArr[i];
-      }
-        
-    },1500);
+    
+    
     return (
     <>
     <hero className="hero">
         <img src={logo} alt="HC logo" className="hero-img"/>
-        <p style={{fontSize:"50px"}}>Let's  {changingText}  Together</p>
+        <p style={{fontSize:"40px"}}>
+            Let's  
+        <Typewriter
+         options={{
+            strings: ["Grow","Build","Learn","Code"],
+            autoStart: true,
+            loop: true,
+          }}
+        />  
+        Together</p>
         <Button id="hero-button">Join Us</Button>
     </hero>
     
