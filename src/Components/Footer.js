923c4854f9901d90c664d2f12f3b73e46961faa8
vikassethi09 <vikassethi2000@gmail.com>
2021-10-26 21:37:38 +0530

    basic footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,16 +4,19 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="footer-items">
-                <h3>HackClub CGC</h3>
+                <p style={{margin:"10px",color:"white", fontSize:"20px"}}><strong>HackClub CGC</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestiae voluptas autem veniam a, laudantium animi voluptate voluptates nulla omnis?</p>
                 
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
             </div>
-            <div className="footer-items"></div>
-            <div className="footer-items"></div>
+            <div className="footer-items"> 
+            <h6>Follow Us</h6>
+            
+            </div>
+            <div className="footer-items">
+                <h6>Contact us</h6> 
+                <p>hackclubcgc@gmail.com</p>
+            
+            </div>
         </div>
     )
 }
