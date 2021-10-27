afbe0adc4e43c7302f7aa3759e2eab6278b51187
vikassethi09 <vikassethi2000@gmail.com>
2021-10-27 22:27:54 +0530

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import './components.css'
 export default function Footer() {
+
+    const follow = ["Insta","Linkedin"]
+    const followlist = follow.map((f)=> <li>{f}</li>)
     return (
         <div className="footer">
             <div className="footer-items">
-                <p style={{margin:"10px",color:"white", fontSize:"20px"}}><strong>HackClub CGC</strong></p>
+                <p style={{marginTop:"10px"}}>About us</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestiae voluptas autem veniam a, laudantium animi voluptate voluptates nulla omnis?</p>
-                
-            </div>
-            <div className="footer-items"> 
-            <h6>Follow Us</h6>
-            
+                <p style={{marginTop:"10px",color:"white", fontSize:"20px"}}><strong>HackClub CGC</strong></p>
             </div>
             <div className="footer-items">
-                <h6>Contact us</h6> 
+            <h6 style={{color:"white"}}>Follow Us</h6>
+            <ul>
+                {followlist}
+            </ul>
+
+            </div>
+            <div className="footer-items">
+                <h6 style={{color:"white"}}>Contact us</h6>
                 <p>hackclubcgc@gmail.com</p>
-            
             </div>
         </div>
     )
