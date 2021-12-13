efde56b9c3506f331c6ea4ffbbd134565d9e4001
vikassethi09 <vikassethi2000@gmail.com>
2021-12-13 18:36:17 +0530

    13-12-2020

--- src/Components/whatwedo.js
+++ src/Components/whatwedo.js
@@ -5,7 +5,7 @@ import './components.css'
 export default function whatwedo() {
     
     return (
-        <div className="Wmain">
+        <div className="Wmain" >
             <h3>What We Do</h3>
             <p><strong>We code, learn and teach together. We believe in open source and innovation. We organize bootcamps, events, community sessions and make projects</strong> </p>
             <div className="Wcards">
@@ -15,11 +15,11 @@ export default function whatwedo() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>  </div>
                 <div className="Wc-items" style={{flexBasis:"33%"}}><div className="ci">
                 <h4>Projects</h4>
-                <img src='https://cloud-ohgqvmh3i-hack-club-bot.vercel.app/0instagram_post_-_5.png' alt='logo' className='cphoto' ></img><br/>
+                <img src='https://cloud-jk9cgimyk-hack-club-bot.vercel.app/06101073.jpg' alt='logo' className='cphoto' ></img><br/>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div></div>
                 <div className="Wc-items" style={{flexBasis:"33%"}}><div className="ci">
                 <h4>Community Meetups</h4>
-                <img src='https://cloud-ohgqvmh3i-hack-club-bot.vercel.app/0instagram_post_-_5.png' alt='logo' className='cphoto' ></img><br/>
+                <img src='https://cloud-jk9cgimyk-hack-club-bot.vercel.app/06101073.jpg' alt='logo' className='cphoto' ></img><br/>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
                     </div></div>
             </div>
--- src/Pages/Aboutus.js
+++ src/Pages/Aboutus.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Aboutus() {
     return (
         <div>
-            About Us
+            
+            About HackClub CGC:-
+"Alone, we can do so little together, we can do so much" - Helen Keller
+Pioneered by three visionary students who wanted better for their fellow coders, HC CGC is one of its kind. 
+We are a group of people pooling our skills to help out each other and ultimately grow together. 
+"We're at our best when we are making" is our motto. All you need to join the club is a learning attitude. 
+We are by the coders and for the coders.
         </div>
     )
 }
