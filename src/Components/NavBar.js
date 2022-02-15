1f1099e37f8f65ce3beb2d29c962621d1f32c4fc
vikassethi09 <vikassethi2000@gmail.com>
2022-02-15 09:12:46 +0530

    updated URl

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
+import NavLink from 'react-router-dom'
 import './components.css'
 import Icon from '@hackclub/icons'
 
@@ -20,12 +20,12 @@ export default function NavBar() {
       <Container>
       <Navbar.Brand href="/" className="nav-brand">HackClub CGC</Navbar.Brand>
       <Nav className={"flex-row nav-items"}>
-        <Link exact to="/" className="nav-item">Home</Link>
-        <Link exact to="/events" className="nav-item">Events</Link>
-        {/* <Link exact to="/projects" className="nav-item">Projects</Link> */}
-        <Link exact to="/team" className="nav-item">Team</Link>
-        <Link exact to="/resources" className="nav-item">Resources</Link>
-        <Link exact to="/contactus" className="nav-item">Contact Us</Link>
+        <NavLink exact to="/" className="nav-item">Home</Link>
+        <NavLink exact to="/events" className="nav-item">Events</Link>
+        {/* <NavLink exact to="/projects" className="nav-item">Projects</Link> */}
+        <NavLink exact to="/team" className="nav-item">Team</Link>
+        <NavLink exact to="/resources" className="nav-item">Resources</Link>
+        <NavLink exact to="/contactus" className="nav-item">Contact Us</Link>
       </Nav>
 
       </Container>
@@ -35,12 +35,12 @@ export default function NavBar() {
 
   </Navbar>
   <Nav className={toggle ? "flex-column list-disp1" : "list-disp" }>
-        <Link exact to="/" className="nav-item">Home</Link>
-        <Link exact to="/events" className="nav-item">Events</Link>
+        <NavLink exact to="/" className="nav-item">Home</Link>
+        <NavLink exact to="/events" className="nav-item">Events</Link>
        {/*  <Link exact to="/projects" className="nav-item">Projects</Link> */}
-        <Link exact to="/team" className="nav-item">Team</Link>
-        <Link exact to="/resources" className="nav-item">Resources</Link>
-        <Link  exact to="/contactus" className="nav-item">Contact Us</Link>
+        <NavLink exact to="/team" className="nav-item">Team</Link>
+        <NavLink exact to="/resources" className="nav-item">Resources</Link>
+        <NavLink  exact to="/contactus" className="nav-item">Contact Us</Link>
   </Nav>
 </>
     )
